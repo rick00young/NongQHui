b0ae33155b00229279bc04dee5c9c30f91be605f
yangyongrui <yyr168@gmail.com>
2015-03-23 00:12:40 +0800

    add good modle

---
+++ application/models/Good.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rick
+ * Date: 15/3/15
+ * Time: 上午9:45
+ */
+
+class GoodModel {
+    const TABLE_NAME = 'good';
+    const INFO_TABLE = 'good_ext_info';
+    //类别:1为采摘,2为柴鸡蛋,3为农家乐,0未知
+    const FARM = 1;
+    const EGG = 2;
+    const YARD = 3;
+    const UNKNOWN = 0;
+
+    public static function createNewGood($goodData){
+        $goodData['add_time'] = time();
+        DB::insert($goodData, self::TABLE_NAME, array('logo'));
+        return DB::lastInsertId();
+    }
+
+    public static function updateGood($goodData, $goodId){
+        return DB::update($goodData, self::TABLE_NAME, $goodId, array('logo'));
+    }
+
+    public static function getGoodsByShopId($goodId)
+    {
+        $sql  = sprintf('SELECT * FROM `%s` ', self::TABLE_NAME);
+        $sql .= sprintf('WHERE `good_id` = "%s" ', DB::escape($goodId));
+        //echo $sql . PHP_EOL;exit;
+
+        return DB::getAll($sql);
+    }
+
+    public static function getGoodById($goodId){
+        $sql  = sprintf('SELECT * FROM `%s` ', self::TABLE_NAME);
+        $sql .= sprintf('WHERE `id` = "%s" ', DB::escape($goodId));
+        //echo $sql . PHP_EOL;exit;
+        return DB::getOne($sql);
+    }
+
+    public static function updateOrInsertGoodExInfoByGoodId($data, $goodId, $type){
+        $exInfoRes = self::getGoodExInfoByGoodId($goodId, $type);
+        if($exInfoRes){
+            return self::updateGoodExInfoByGoodId($data, $exInfoRes['id']);
+        }
+        return self::createGoodExInfo($data);
+    }
+
+    public static function getGoodExInfoByGoodId($goodId, $type){
+        $sql  = sprintf('SELECT * FROM `%s` ', self::INFO_TABLE);
+        $sql .= sprintf('WHERE `good_id` = "%d" and type = "%d', DB::escape($goodId), DB::escape($type));
+        //echo $sql . PHP_EOL;exit;
+        return DB::getOne($sql);
+    }
+
+    public static function updateGoodExInfoByGoodId($data, $id){
+        return DB::update($data, self::INFO_TABLE, $id);
+    }
+
+    public static function createGoodExInfo($data){
+        $data['add_time'] = time();
+        DB::insert($data, self::TABLE_NAME, array('logo'));
+        return DB::lastInsertId();
+    }
+} 
--- static/admin/js/goodMoreInfo.js
+++ static/admin/js/goodMoreInfo.js
@@ -1,21 +1,30 @@
-function getPicTpl($src, $md5, $ext){
+    function getPicTpl($src, $md5, $ext){
+        var button = $('<button class="btn btn-danger btn-xs"><i class="fa fa-times"></i></button>');
+        button.click(function(){
+        //TODO删除图片数据
+            alert('delete data...');
+            return false;
+        });
+        var $tpl = $('<div class="col-md-3">'
+                        + '<div class="uploaded_pic">'
+                        + '<img src="'+$src+'" alt="..." class="img-thumbnail">'
+                            //+ '<button class="btn btn-danger btn-xs"><i class="fa fa-times"></i></button>'
+                            + '</div>'
+                    + '</div>');
 
-    var button = $('<button class="btn btn-danger btn-xs"><i class="fa fa-times"></i></button>');
-    button.click(function(){
-    //TODO删除图片数据
-        alert('delete data...');
-        return false;
-    });
-    var $tpl = $('<div class="col-md-3">'
-                    + '<div class="uploaded_pic">'
-                    + '<img src="'+$src+'" alt="..." class="img-thumbnail">'
-                        //+ '<button class="btn btn-danger btn-xs"><i class="fa fa-times"></i></button>'
-                        + '</div>'
-                + '</div>');
+        $tpl.find('.uploaded_pic').append(button);
+        return $tpl;
+    }
+    //ueditor
+    var editor = UE.getEditor('editor');
 
-    $tpl.find('.uploaded_pic').append(button);
-    return $tpl;
-}
+    var GOODINFO = {
+        'good_intro': good_intro,
+        'buy_needKnow': buy_needKnow,
+        'buy_detail': buy_detail,
+        'use_list': use_list
+    };
+    var CURRENT_OPERATION = 'good_intro';
 
     $("#good_photo").uploadify({
         'swf'           : '/static/plugin/uploadify/uploadify.swf',
@@ -31,60 +40,89 @@ function getPicTpl($src, $md5, $ext){
             //alert('The file ' + file.name + ' was successfully uploaded with a response of ' + response + ':' + data);
             //{"status":1,"data":{"code":0,"info":"uplode success!","md5":"489bd2daea761d3d65019bbe2e9ff7cc","ext":"jpg"}}
 
-        var res = $.parseJSON(data);
-        if(1 != res.status){
-            alert('图片上传出问题了....');
-            return;
-        }
-        src = '/source/images/thb2/100x100/' + res.data.dir + '/' +res.data.md5 + '.' + res.data.ext;
-        var picTpl =getPicTpl(src);
-        $('#goodPhotoBox').find('.row').append(picTpl);
-            logoJson = JSON.stringify({'md5':res.data.md5, 'ext': res.data.ext});
+            var res = $.parseJSON(data);
+            if(1 != res.status){
+                alert('图片上传出问题了....');
+                return;
+            }
+            src = '/source/images/thb2/100x100/' + res.data.dir + '/' +res.data.md5 + '.' + res.data.ext;
+            var picTpl =getPicTpl(src);
+            $('#goodPhotoBox').find('.row').append(picTpl);
+            //logoJson = JSON.stringify({'md5':res.data.md5, 'ext': res.data.ext});
         }
     });
 
     $("#submit").click(function () {
-        var shop_name =   $("#shop_name").val();
-        var shop_slogan =   $("#shop_slogan").val();
-        var shop_des =    $("#shop_des").val();
-        var shop_lat  =   $("#shop_lat").val();
-        var shop_long =   $("#shop_long").val();
-        var shop_id = $('#shop_id').val();
+        var good_name =   $("#good_name").val();
+        var good_slogan =   $("#good_slogan").val();
+        var good_des =    $("#good_des").val();
+        var good_lat  =   $("#good_lat").val();
+        var good_long =   $("#good_long").val();
+        var good_id = $('#good_id').val();
 
-        if(!shop_name || !shop_slogan || !shop_des || !shop_lat || !shop_long){
+        if(!good_name || !good_slogan || !good_des || !good_lat || !good_long){
             alert("您填入的数据不完整,请你检查后重新提交!");
             return false;
         }
 
         $.post("/admin/savegood", {
-            'shop_name':shop_name,
-            'shop_slogan':shop_slogan,
-            'shop_des': shop_des,
-            'shop_lat': shop_lat,
-            'shop_long': shop_long,
-            'shop_logo':logoJson,
-            'shop_id' : shop_id
+            'good_name':good_name,
+            'good_slogan':good_slogan,
+            'good_des': good_des,
+            'good_lat': good_lat,
+            'good_long': good_long,
+            'good_logo':logoJson,
+            'good_id' : good_id
             },function(response){
             //console.log(response);
 
             if(0 == response.errno){
             alert('保存成功.');
-                location.href = '/admin/shoplist';
+                location.href = '/admin/goodlist';
             }else{
                 alert('好像出问题了.');
             }
         })
     })
 
-    $('#goodSegment_box').find('.goodSegment').click(function(){
+    var $segments = $('#goodSegment_box').find('.goodSegment');
+    $segments.click(function(){
         var data_title = $(this).attr('data-title');
-        var segments = {'buy_needKnow':true,'good_intro':true, 'buy_detail':true,'use_list':true};
-        if(segments[data_title] && data_title == 'good_intro'){
-            $('#')
-        }
+        var segments = {'buy_needKnow':'购买须知','good_intro':'good_intro', 'buy_detail':'订购详情','use_list':'使用流程'};
 
+
+        if(segments[data_title] && data_title == 'good_intro'){
+            $('#upload, #goodPhotoBox').slideDown();
+            $('#info_editor').slideUp();
+        }else{
+            //editor
+            $('#upload, #goodPhotoBox').slideUp();
+            $('#info_editor').slideDown();
+            $('#segment_title').text(segments[data_title]);
+
+        }
+        saveCacheDataByType(CURRENT_OPERATION);
+        CURRENT_OPERATION = data_title;
+        getCacheDataByType(CURRENT_OPERATION);
+
+        $segments.removeClass('btn-info').addClass('btn-default');
+        $(this).addClass('btn-info').removeClass('btn-default');
     });
 
 
-    //ueditor
-    var ue = UE.getEditor('editor');
+
+
+    function saveCacheDataByType(type){
+        if('good_intro' == type){
+
+        }else{
+            GOODINFO[type] = editor.getContent();
+            //console.log(GOODINFO[type]);
+        }
+
+    }
+
+    function getCacheDataByType(type){
+        var content = GOODINFO[type];
+        editor.setContent(content);
+    }
