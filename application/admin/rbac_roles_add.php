240f6d44308d8d123d9a369b11bd99dc233db765
Jerry Yang <hy0kle@gmail.com>
2015-03-25 14:27:55 +0800

    完成新增角色功能 :floppy_disk:

---
+++ application/admin/rbac_roles_add.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @describe:
+ * @author: Jerry Yang(hy0kle@gmail.com)
+ * */
+class rbac_roles_addAction extends BaseAction
+{
+    public function run($arg = null)
+    {
+        $title       = '';
+        $description = '';
+        $redirect    = '/admin/rbac_roles_view';
+
+        do {
+            if (! isset($_REQUEST['title']) || strlen($_REQUEST['title']) <= 0
+                || ! isset($_REQUEST['description']) || strlen($_REQUEST['description']) <= 0
+            )
+            {
+                Html::setFlash('必填项目不能为空');
+                break;
+            }
+
+            $title       = DB::escape($_REQUEST['title']);
+            $description = DB::escape($_REQUEST['description']);
+
+            /** 检查是否已存在,防止重复数据 */
+            $rbac = RbacService::getInstance();
+            $rid  = $rbac->Roles->titleId($title);
+            if ($rid > 1)
+            {
+                Html::setFlash($title . ' 已存在');
+                break;
+            }
+
+            $rbac->Roles->add($title, $description);
+            Html::setFlash('新角色增加成功');
+        } while (0);
+
+        $this->redirect($redirect);
+        return false;
+    }
+}
+/* vi:set ts=4 sw=4 et fdm=marker: */
+
