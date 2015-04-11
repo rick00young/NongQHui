c673941c4aedd48597d288b186ed19f13169432e
Jerry Yang <hy0kle@gmail.com>
2015-04-11 12:01:55 +0800

    加入 debug_backtrace()

--- application/library/Util.php
+++ application/library/Util.php
@@ -10,5 +10,12 @@ class Util
         $blk = substr($str, 0, 512);
         return (substr_count($blk, "\x00") > 0);
     }
+
+    public static function bt($file, $line, $e)
+    {
+        SeasLog::debug(sprintf('[BT-BEGIN] %s: %s {{{', $file, $line));
+        SeasLog::debug((string)$e);
+        SeasLog::debug(sprintf('[BT-END] %s: %s }}}', $file, $line));
+    }
 }
 /* vi:set ts=4 sw=4 et fdm=marker: */
