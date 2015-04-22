8007bd8f0f2336885775c363f16f59b3dafa8b47
Jerry Yang <hy0kle@gmail.com>
2015-04-22 11:28:18 +0800

    php 层增加了判断用户是不是在微信中

--- application/library/Widget.php
+++ application/library/Widget.php
@@ -19,6 +19,25 @@ class Widget
         $rdb = RedisDB::getInstance();
         $rdb->incr(self::good_pv_ckey . $gid);
     }
+
+    public static function isWeixinBrowser()
+    {
+        $ret = false;
+        $ua  = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : '';
+
+        if ('' == $ua)
+        {
+            return $ret;
+        }
+
+        $p_ret = preg_match('/MicroMessenger/i', $ua, $matches);
+        if ($p_ret)
+        {
+            $ret = true;
+        }
+
+        return $ret;
+    }
 }
 /* vi:set ts=4 sw=4 et fdm=marker: */
 
